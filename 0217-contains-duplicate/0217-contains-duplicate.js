a29e92e1b177d76161f1155905ccc1556bb1ffb0
Me_Asad <93114618+MuhammadAsadAnsari@users.noreply.github.com>
2024-02-23 21:52:52 +0500

    Time: 78 ms (69.44%), Space: 60.5 MB (49.11%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    const  myset  = new Set();
+
+for(let i =0;i<nums.length;i++){
+    if(myset.has(nums[i])){
+        return true
+    }
+    myset.add(nums[i])
+}
+return false
+};
